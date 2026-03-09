ebfd9d91fda3dfa1c24186c1b57ca6129a6de867
Rick <rsulisthio@gmail.com>
2026-03-09 04:19:10 +0000

    fix: burner wallet connector broken

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export {
   createBurnerWallet,
   burnerWalletId,
   burnerWalletName,
+  burnerWalletIcon,
 } from "./connector";
 export * from "./errors";
 export { burnerAccounts, type BurnerAccount } from "./devnetAccounts";
